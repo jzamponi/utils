a72e88a9bbbc8651d12b8e1e6727b392235a1b03
Joaquin Zamponi <jzamponi@mpe.mpg.de>
2022-03-19 22:10:37 +0100

    CASA script for polarized ALMA Band 6 syn. obs.

--- synthetic_observations/1.3mm/alma_polarization_simulation.py
+++ synthetic_observations/1.3mm/alma_polarization_simulation.py
@@ -1,6 +1,8 @@
 # Setup based on Sadavoy et al. (2018b)
 
 import time
+import random
+
 Simobserve = True
 Clean = True
 polarization = True
@@ -23,6 +25,7 @@ if Simobserve:
         obsmode = 'int',
         antennalist = 'alma.cycle3.5.cfg',
         thermalnoise = 'tsys-atm',
+        seed = int(random.random() * 100),
         graphics = 'both',
         overwrite = True,
         verbose = True
@@ -43,6 +46,7 @@ if Simobserve:
             obsmode = 'int',
             antennalist = 'alma.cycle3.5.cfg',
             thermalnoise = 'tsys-atm',
+            seed = int(random.random() * 100),
             graphics = 'file',
             overwrite = True,
             verbose = True
@@ -62,6 +66,7 @@ if Simobserve:
             obsmode = 'int',
             antennalist = 'alma.cycle3.5.cfg',
             thermalnoise = 'tsys-atm',
+            seed = int(random.random() * 100),
             graphics = 'file',
             overwrite = True,
             verbose = True
@@ -82,11 +87,11 @@ if Clean:
         weighting = 'briggs',
         robust = 0.5,
         uvtaper = '0.1arcsec',
-        mask = 'band6_I/band6_I.alma.cycle3.5.skymodel',
+        mask = 'centerbox[[200pix, 200pix], [50pix, 50pix]]',
         niter = 10000,
-        threshold = '250uJy',
+        threshold = '280uJy',
         pbcor = True, 
-        interactive = True,
+        interactive = False,
         verbose = True
     )
     if polarization:
@@ -103,9 +108,9 @@ if Clean:
             weighting = 'briggs',
             robust = 0.5,
             uvtaper = '0.1arcsec',
-            mask = 'band6_I/band6_I.alma.cycle3.5.skymodel',
+            mask = 'centerbox[[200pix, 200pix], [50pix, 50pix]]',
             niter = 10000,
-            threshold = '100uJy',
+            threshold = '25uJy',
             pbcor = True, 
             interactive = False,
             verbose = True
@@ -123,9 +128,9 @@ if Clean:
             weighting = 'briggs',
             robust = 0.5,
             uvtaper = '0.1arcsec',
-            mask = 'band6_I/band6_I.alma.cycle3.5.skymodel',
+            mask = 'centerbox[[200pix, 200pix], [50pix, 50pix]]',
             niter = 10000,
-            threshold = '100uJy',
+            threshold = '25uJy',
             pbcor = True, 
             interactive = False,
             verbose = True
@@ -133,15 +138,15 @@ if Clean:
 
     imregrid('band6_I/clean_I.image', template='band6_I/band6_I.alma.cycle3.5.skymodel.flat', \
         output='band6_I/clean_I.image_modelsize', overwrite=True)
-    exportfits('band6_I/clean_I.image_modelsize', fitsimage='alma_I.fits', dropstokes=True, overwrite=True)
+    exportfits('band6_I/clean_I.image_modelsize', fitsimage='alma_I.fits', dropdeg=True, overwrite=True)
 
     if polarization:
         imregrid('band6_Q/clean_Q.image', template='band6_Q/band6_Q.alma.cycle3.5.skymodel.flat', \
             output='band6_Q/clean_Q.image_modelsize', overwrite=True)
         imregrid('band6_U/clean_U.image', template='band6_U/band6_U.alma.cycle3.5.skymodel.flat', \
             output='band6_U/clean_U.image_modelsize', overwrite=True)
-        exportfits('band6_Q/clean_Q.image_modelsize', fitsimage='alma_Q.fits', overwrite=True, dropstokes=True)
-        exportfits('band6_U/clean_U.image_modelsize', fitsimage='alma_U.fits', overwrite=True, dropstokes=True)
+        exportfits('band6_Q/clean_Q.image_modelsize', fitsimage='alma_Q.fits', dropdeg=True, overwrite=True)
+        exportfits('band6_U/clean_U.image_modelsize', fitsimage='alma_U.fits', dropdeg=True, overwrite=True)
 
 
 print('\n[alma_simulaton] Elapsed time: {}'\
