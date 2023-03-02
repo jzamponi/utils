d271cde82067839c7fbd49fa8dfa1009df459f8d
Joaquin Zamponi <jzamponi@mpe.mpg.de>
2023-03-02 11:56:28 +0100

    updated dust opacities

--- opacity_tables/calculate.sh
+++ opacity_tables/calculate.sh
@@ -1,28 +1,52 @@
+#for a in 10 100 1000
+#do
+#    for material in "s" "sg"
+#    do
+#        synthesizer \
+#            --opacity \
+#            --nang 181 \
+#            --na 200 \
+#            --overwrite \
+#            --amax ${a} \
+#            --material ${material} \
+#            --nopb
+#
+#        synthesizer \
+#            --opacity \
+#            --nang 181 \
+#            --na 200 \
+#            --overwrite \
+#            --amax ${a} \
+#            --material ${material} \
+#            --polarization \
+#            --nopb
+#    done
+#done
+
+# Cases with organics and sublimation
 for a in 10 100 1000
 do
-#    for material in "s" "sg" "sgo"
-    for material in "sgo"
+    for org in 10 30 50 80
     do
-        for csubl in "30" "50" "80"
-        do
-            synthesizer \
-                --opacity \
-                --nang 181 \
-                --na 200 \
-                --overwrite \
-                --amax ${a} \
-                --material ${material} \
-                --sublimation ${csubl} 
+        synthesizer \
+            --opacity \
+            --nang 181 \
+            --na 200 \
+            --overwrite \
+            --amax ${a} \
+            --material sgo \
+            --sublimation ${org} \
+            --nopb
 
-            synthesizer \
-                --polarization \
-                --opacity \
-                --nang 181 \
-                --na 200 \
-                --overwrite \
-                --amax ${a} \
-                --material ${material} \
-                --sublimation ${csubl} 
-        done            
+        synthesizer \
+            --opacity \
+            --nang 181 \
+            --na 200 \
+            --overwrite \
+            --amax ${a} \
+            --material sgo \
+            --polarization \
+            --sublimation ${org} \
+            --nopb
     done
 done
